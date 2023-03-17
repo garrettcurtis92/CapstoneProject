3a9f724b52120de69eaa4b2b5911fb6abf5f6d9e
gcurtis1092@gmail.com <gcurtis1092@gmail.com>
2023-03-17 16:11:47 -0600

    hamburger menu

--- components/Nav.js
+++ components/Nav.js
@@ -2,6 +2,8 @@ import html from "html-literal";
 
 export default links => html`
   <nav>
+    <i class="fas fa-bars"></i>
+    <ul class="hidden--mobile nav-links">
       ${links
         .map(
           link =>
