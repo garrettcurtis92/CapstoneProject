a7d69886ed27d340b4b66494cb13fcc8f8ef57eb
gcurtis1092@gmail.com <gcurtis1092@gmail.com>
2023-03-21 19:25:43 -0600

    trainerpic added

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,10 +1,17 @@
 import html from "html-literal";
 import nomadLogo from "../../assets/img/nomad-coach.png";
-
+import trainerPic from "../../assets/img/trainer.png";
 export default () => html`
+
+<div>
+
+</div>
   <section class="searchBar">
+
     <div class="header">
+
       <div class="logoContainer">
+      <img class="trainerPic" src="${trainerPic}" />
         <img class="logo" src="${nomadLogo}"/>
       </div>
       <div class="search_container">
